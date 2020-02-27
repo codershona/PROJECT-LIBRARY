5933b7c4cf7716105ceb8fd718de7c74bb8f0121
codershona <falgunimst95@gmail.com>
2020-02-27 18:49:17 +0100

    working on add book UI

--- app.js
+++ app.js
@@ -57,4 +57,29 @@ document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a BOOKS
 
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+// Prevent actual submit
+
+e.preventDefault();
+
+  // Get form values
+
+  const title = document.querySelector('#title').value;
+
+  const author = document.querySelector('#author').value;
+
+  const isbn = document.querySelector('#isbn').value;
+
+// Instatiate books
+
+const book = new Book(title, author, isbn);
+
+ // console.log(book)
+
+ // Add Book to UI :
+
+ UI.addBookToList(book);
+});
+
 // Event : Remove a BOOKS
